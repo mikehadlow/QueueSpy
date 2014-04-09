4fa932c9d9bc69acdafb2104cdde5595b230969c
Mike Hadlow <mike@suteki.co.uk>
2014-04-09 09:26:36 +0000

    Some console output for the node CI server.

--- node-ci.js
+++ node-ci.js
@@ -3,6 +3,7 @@ var util = require('util');
 var exec = require('child_process').exec;
 
 http.createServer( function(req, res) {
+   console.log('starting CI build request.');
 
    var child = exec('ci/ci-bootstrapper.sh', function (error, stdout, stderr) {
         res.writeHead(200, {'Content-Type': 'text/plain' });
@@ -13,6 +14,9 @@ http.createServer( function(req, res) {
         res.write('stderr: ' + stderr + '\n');
         if(error !== null) {
             res.write('exec error: ' + error);
+            console.log('CI completed with errors: ' + error);
+        } else {
+            console.log('CI completed successfully.');
         }
         
         res.end();
