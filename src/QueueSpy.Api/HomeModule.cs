3cd19a458c814bd40c38aa4264370d530407fbb8
Mike Hadlow <mike@suteki.co.uk>
2014-04-09 12:08:14 +0100

    QueueSpy.Api - Added NancyFx self host dependencies and initial HomeModule.

---
+++ src/QueueSpy.Api/HomeModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Nancy;
+
+namespace QueueSpy.Api
+{
+	public class HomeModule : NancyModule
+	{
+		public HomeModule ()
+		{
+			Get ["/"] = parameters => "Hello from QueueSpy.Api!";
+		}
+	}
+}
+
--- src/QueueSpy.Api/Program.cs
+++ src/QueueSpy.Api/Program.cs
@@ -1,9 +1,30 @@
-﻿namespace QueueSpy.Api
+﻿using System;
+using System.Threading;
+using Nancy.Hosting.Self;
+
+namespace QueueSpy.Api
 {
     class Program
     {
         static void Main(string[] args)
         {
+			var nancyHost = new NancyHost (new Uri ("http://localhost:8888/"));
+			nancyHost.Start ();
+
+			Console.WriteLine ("Nancy listening on port 8888. Hit <Return> to close.");
+
+			var are = new AutoResetEvent (false);
+
+			Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) => {
+				// cancel the cancellation to allow the program to shutdown cleanly
+				e.Cancel = true;
+				are.Set();
+			};
+
+			are.WaitOne ();
+			nancyHost.Stop ();
+
+			Console.WriteLine ("Nancy host closed.");
         }
     }
 }
