094390cde76f7a70df66d4b10ea3b92e8204b788
Mike Hadlow <mike@suteki.co.uk>
2014-04-14 10:43:14 +0100

    Users now displayed on console, Db connection string in App.config.

--- website/sites/console/js/controllers.js
+++ website/sites/console/js/controllers.js
@@ -1,10 +1,34 @@
 
 // QueueSpy controllers
 
-var queuespyApp = angular.module('queuespyApp', []);
+var queuespyApp = angular.module('queuespyApp', ['ngRoute', 'queuespyControllers']);
 
-queuespyApp.controller('VersionController', function ($scope, $http) {
+queuespyApp.config(['$routeProvider', function ($routeProvider){
+	$routeProvider.
+		when('/version', {
+			templateUrl: 'html/version.html',
+			controller: 'VersionController'
+		}).
+		when('/users', {
+			templateUrl: 'html/users.html',
+			controller: 'UserController'
+		}).
+		otherwise({
+			redirectTo: '/version'
+		});
+}]);
+
+var queuespyControllers = angular.module('queuespyControllers', []);
+
+
+queuespyControllers.controller('VersionController', function ($scope, $http) {
 	$http.get('/api/version').success(function(data){
 		$scope.version = data;
+	});
+});
+
+queuespyControllers.controller('UserController', function ($scope, $http) {
+	$http.get('/api/user').success(function(data){
+		$scope.users = data;
 	});
 });
