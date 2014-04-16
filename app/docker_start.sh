931135a3b193ddadbec1464464773167a297b5d6
Mike Hadlow <mike@suteki.co.uk>
2014-04-16 11:15:32 +0000

    Start all app processes with supervisor.

--- app/docker_start.sh
+++ app/docker_start.sh
@@ -4,4 +4,4 @@
 
 sed -i "s/host=localhost/host=$RABBIT_PORT_5672_TCP_ADDR;username=admin;password=i8rUx_32mn/g" src/*/bin/Debug/*.config
 
-mono src/QueueSpy.Api/bin/Debug/QueueSpy.Api.exe
+supervisord -n -c supervisord.conf
