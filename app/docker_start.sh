57947607eb97495b109c1812a70f704bbf575519
Mike Hadlow <mike@suteki.co.uk>
2014-04-16 10:56:06 +0000

    Correct username and password for docker RabbitMQ broker.

--- app/docker_start.sh
+++ app/docker_start.sh
@@ -2,6 +2,6 @@
 
 # This script is run when the queuespy_app docker container starts
 
-sed -i "s/host=localhost/host=$RABBIT_PORT_5672_TCP_ADDR/g" src/*/bin/Debug/*.config
+sed -i "s/host=localhost/host=$RABBIT_PORT_5672_TCP_ADDR;username=admin;password=i8rUx_32mn/g" src/*/bin/Debug/*.config
 
 mono src/QueueSpy.Api/bin/Debug/QueueSpy.Api.exe
