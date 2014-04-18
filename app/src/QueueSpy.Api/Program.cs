ad9250ed0ed37ce696cf21b4046831347c87ea0c
Mike Hadlow <mike@suteki.co.uk>
2014-04-18 15:23:49 +0100

    Changed Queuespy.Api to use OWIN.

--- app/src/QueueSpy.Api/Program.cs
+++ app/src/QueueSpy.Api/Program.cs
@@ -1,39 +1,48 @@
 ﻿using System;
 using System.Threading;
-using Nancy.Hosting.Self;
 using Nancy;
+using Microsoft.Owin.Hosting;
+using Owin;
 
 namespace QueueSpy.Api
 {
     class Program
     {
-		private static string url = "http://localhost:8080/";
+		private const string url = "http://localhost:8080/";
 
         static void Main(string[] args)
         {
 			StaticConfiguration.DisableErrorTraces = false;
-
-			var nancyHost = new NancyHost (new Uri (url));
-			nancyHost.Start ();
-
 			var heartbeatMonitor = new HeartbeatMonitor ();
-			heartbeatMonitor.Start ();
 
-			Console.WriteLine ("Nancy listening on {0}. Hit <Return> to close.", url);
+			using (WebApp.Start<Startup> (url)) {
+				heartbeatMonitor.Start ();
+				Console.WriteLine ("Nancy listening on {0}. Ctrl-C to stop.", url);
+				BlockUntilStopSignal ();
+			}
 
+			heartbeatMonitor.Stop ();
+
+			Console.WriteLine ("Nancy host closed.");
+        }
+
+		static void BlockUntilStopSignal()
+		{
 			var are = new AutoResetEvent (false);
-
 			Console.CancelKeyPress += (object sender, ConsoleCancelEventArgs e) => {
 				// cancel the cancellation to allow the program to shutdown cleanly
 				e.Cancel = true;
 				are.Set();
 			};
-
 			are.WaitOne ();
-			nancyHost.Stop ();
-			heartbeatMonitor.Stop ();
-
-			Console.WriteLine ("Nancy host closed.");
-        }
+		}
     }
+
+	class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.UseNancy ();
+		}
+	}
 }
