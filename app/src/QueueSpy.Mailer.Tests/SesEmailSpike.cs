50ca6bf1a5704edfa5ee8918774e44d331df857a
Mike Hadlow <mike@suteki.co.uk>
2014-05-06 13:45:47 +0100

    AWS client libraries (not available in NuGet)

---
+++ app/src/QueueSpy.Mailer.Tests/SesEmailSpike.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+using System.Net.Mail;
+using System.Net;
+
+namespace QueueSpy.Mailer.Tests
+{
+	[TestFixture ()]
+	public class SesEmailSpike
+	{
+		private const string userName = "AKIAIYVDQNDVBIHKBVGQ";
+		private const string password = "AuGMKBrcFge0JPpJuIrmZQyoU70EPSsl2VDdlOWxbhGq";
+
+		[Test]
+		public void Should_be_able_to_send_an_email_via_Amazon_SES_using_SMTP ()
+		{
+			using (var smtp = new SmtpClient {
+				Host = "email-smtp.us-east-1.amazonaws.com",
+				Port = 587,
+				EnableSsl = true,
+				Credentials = new NetworkCredential (userName, password)
+			}) {
+				using (var message = new MailMessage ("info@queuespy.com", "info@queuespy.com") {
+					Subject = "Hello from the unit test!",
+					Body = "And this is the message body. It's not HTML (yet)."
+				}) {
+					smtp.Send (message);
+				}
+			}
+		}
+	}
+}
+
