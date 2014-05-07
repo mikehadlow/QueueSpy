549b6a67d339ff47090d33c23d958b5199ac9eba
Mike Hadlow <mike@suteki.co.uk>
2014-05-07 15:04:41 +0100

    Correct removal order for containers.

--- ci/ci.sh
+++ ci/ci.sh
@@ -13,24 +13,29 @@ docker build -t queuespy/website --no-cache=true website/
 docker build -t queuespy/app --no-cache=true app/
 docker build -t queuespy/db --no-cache=true db/
 
-# start the Postgres DB container
+# stop and remove containers in correct dependency order
+docker stop queuespy_website
+docker rm queuespy_website
+
+docker stop queuespy_app
+docker rm queuespy_app
+
+docker stop queuespy_rabbit
+docker rm queuespy_rabbit
+
 docker stop queuespy_db
 docker rm queuespy_db
+
+# start the Postgres DB container
 docker run -d --name queuespy_db queuespy/db
 
 # start the RabbitMQ container
-docker stop queuespy_rabbit
-docker rm queuespy_rabbit
 docker run -d -p 55672:55672 --name queuespy_rabbit -e RABBITMQ_PASS="i8rUx_32mn" tutum/rabbitmq
 
 # start the application container
-docker stop queuespy_app
-docker rm queuespy_app
 docker run -d --link queuespy_rabbit:rabbit --link queuespy_db:db --name queuespy_app queuespy/app
 
 # start the website container
-docker stop queuespy_website
-docker rm queuespy_website
 docker run -d --link queuespy_app:app --name queuespy_website -p 80:80 -p 443:443 queuespy/website
 
 # remove all untagged images
