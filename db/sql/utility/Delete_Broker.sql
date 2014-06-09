83af727478d2b170a49273c75cac0a11d3a3766c
Mike Hadlow <mike@suteki.co.uk>
2014-06-09 10:51:42 +0100

    script to delete a broker.

---
+++ db/sql/utility/Delete_Broker.sql
@@ -0,0 +1,11 @@
+﻿-- set the broker Id that you want to delete here first.
+set @brokerId = 10000;
+
+delete from "QueueLevel" where QueueId in (select QueueId from "Queue" where BrokerId = @brokerid);
+delete from "Queue" where BrokerId = @brokerId;
+delete from "Consumer" where ConnectionId in (select ConnectionId from "Connection" where BrokerId = @brokerId);
+delete from "ClientProperty" where ConnectionId in (select ConnectionId from "Connection" where BrokerId = @brokerId);
+delete from "Connection" where BrokerId = @brokerId;
+delete from "BrokerEvent" where BrokerId = @brokerId;
+delete from "BrokerStatus" where BrokerId = @brokerId;
+delete from "Broker" where id = @brokerId;
