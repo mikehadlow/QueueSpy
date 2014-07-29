7462288f710fdfce59ad7fbee66da8da258b4bb4
Mike Hadlow <mike@suteki.co.uk>
2014-07-29 09:24:54 +0100

    command pipeline now has exchange.

--- app/src/QueueSpy/EasyNetQExtensions.cs
+++ app/src/QueueSpy/EasyNetQExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using EasyNetQ;
+using EasyNetQ.Topology;
 
 namespace QueueSpy
 {
@@ -6,7 +7,27 @@ namespace QueueSpy
 	{
 		public static void SendCommand<T>(this IBus bus, T command) where T : class
 		{
-			bus.Send (QueueSpy.Messages.QueueSpyQueues.CommandQueue, command);
+			bus.ConfigureCommandTopology ();
+            var wrappedMessage = new Message<T>(command);
+			var exchange = new Exchange (QueueSpy.Messages.QueueSpyQueues.CommandQueue);
+			bus.Advanced.Publish(exchange, typeof(T).Name, false, false, wrappedMessage);
+		}
+
+		private static bool topologyConfigured = false;
+		private readonly static object topologyConfigurationLock = new object();
+
+		public static void ConfigureCommandTopology(this IBus bus)
+		{
+			if (topologyConfigured) return;
+
+			lock (topologyConfigurationLock) {
+				if (topologyConfigured) return;
+
+				var queue = bus.Advanced.QueueDeclare (QueueSpy.Messages.QueueSpyQueues.CommandQueue);
+				var exchange = bus.Advanced.ExchangeDeclare (QueueSpy.Messages.QueueSpyQueues.CommandQueue, ExchangeType.Topic);
+				bus.Advanced.Bind (exchange, queue, "#");
+				topologyConfigured = true;
+			}
 		}
 	}
 }
